58fe05598cb06f1543eead8a5010d3fa497fb5fb
GiovanyFT <giovanyfrossard@gmail.com>
2023-08-31 00:00:49 -0300

    Colocando um timeout no serviço

--- lib/services/soma_service.dart
+++ lib/services/soma_service.dart
@@ -1,5 +1,7 @@
 
 
+import 'dart:async';
+
 import 'package:http/http.dart' as http;
 import 'package:somawebservice/services/api_response.dart';
 class SomaService{
@@ -14,14 +16,16 @@ class SomaService{
       };
 
       final _myUri = Uri.parse(_url);
-      var _response = await http.post(_myUri, body: _params);
+      var _response = await http.post(_myUri, body: _params).timeout(Duration(seconds: 3));
 
       // Solicitação feita com sucesso
       if(_response.statusCode == 200)
         return ApiResponse.ok(_response.body);
       else
         return ApiResponse.error("Status do erro emitido pelo servidor: ${_response.statusCode}");
-    } on Exception catch (erro){
+    }on TimeoutException{
+      return ApiResponse.error("Tempo limite excedido");
+    }on Exception catch (erro){
       return ApiResponse.error("Erro na conexão: ${erro.toString()}");
     }
   }
